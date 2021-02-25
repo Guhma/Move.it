102a4257a99ebaaa0adb9f30143365133d42528b
Guhma <gustavoalmeida123490@gmail.com>
2021-02-25 01:30:08 -0300

    Alterações 3º dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
+
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
-import styles from '../styles/pages/Home.module.css'
+import styles from '../styles/pages/Home.module.css';
+
 
 
 export default function Home() {
@@ -22,9 +26,8 @@ export default function Home() {
         <CompletedChallenges />
         <Countdown />
         </div>
-
         <div>
-
+          <ChallengeBox />
 
         </div>
       </section>
